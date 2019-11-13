647b8a3a9edd0571bbf3da0bea6da2764775d0d9
Abu Kashem <akashem@redhat.com>
2019-11-13 14:37:48 -0500

    Make a skeleton mutating admission webhook

---
+++ cmd/cluster-resource-override-admission/main_test.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"testing"
+
+	"github.com/openshift/generic-admission-server/pkg/apiserver"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMutatingHook_MutatingResource(t *testing.T) {
+	pluralWant := schema.GroupVersionResource{
+		Group:    "admission.autoscaling.openshift.io",
+		Version:  "v1",
+		Resource: "clusterresourceoverrides",
+	}
+	singularWant := "clusterresourceoverride"
+
+	var hook apiserver.MutatingAdmissionHook = &mutatingHook{}
+
+	pluralGot, singularGot := hook.MutatingResource()
+
+	assert.Equal(t, pluralWant, pluralGot)
+	assert.Equal(t, singularWant, singularGot)
+}
