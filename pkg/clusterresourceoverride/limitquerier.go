fbea0fd1d20df0d163ca3b91ce4e55d407ff9560
Abu Kashem <akashem@redhat.com>
2020-02-10 18:46:53 -0500

    Overridden quantity must be below namespace Maximum
    The overridden quantity must be below the namespace Maximum specified
    via LimitRange object.
    
    Currently the mutating admission webhook overrides a resource above
    the namespace maximum and thus fails Pod creation, we see the
    following error:
    "pods "croe2e-f7c9h" is forbidden: maximum cpu usage per Container is 1, but limit is 2"
    
    Query the Maximum limit for CPU and Memory resource specified in
    LimitRange and ensure that the overridden resource quantity never
    exceeds the namespace Maximum.

--- pkg/clusterresourceoverride/limitquerier.go
+++ pkg/clusterresourceoverride/limitquerier.go
@@ -13,55 +13,91 @@ type namespaceLimitQuerier struct {
 	limitRangesLister corev1listers.LimitRangeLister
 }
 
-func (l *namespaceLimitQuerier) QueryMinimum(namespace string) (minimum *Floor, err error) {
-	limits, listErr := l.limitRangesLister.LimitRanges(namespace).List(labels.Everything())
+func (l *namespaceLimitQuerier) QueryFloorAndCeiling(namespace string) (floor *CPUMemory, ceiling *CPUMemory, err error) {
+	limitRanges, listErr := l.limitRangesLister.LimitRanges(namespace).List(labels.Everything())
 	if listErr != nil {
 		err = fmt.Errorf("failed to query limitrange - %v", listErr)
 		return
 	}
 
-	nsCPUFloor := minResourceLimits(limits, corev1.ResourceCPU)
-	nsMemFloor := minResourceLimits(limits, corev1.ResourceMemory)
+	nsCPUMinimum, nsCPUMaximum := GetMinMax(limitRanges, corev1.ResourceCPU)
+	nsMemMinimum, nsMemMaximum := GetMinMax(limitRanges, corev1.ResourceMemory)
 
-	minimum = &Floor{
-		CPU:    nsCPUFloor,
-		Memory: nsMemFloor,
+	floor = &CPUMemory{
+		CPU:    nsCPUMinimum,
+		Memory: nsMemMinimum,
+	}
+	ceiling = &CPUMemory{
+		CPU:    nsCPUMaximum,
+		Memory: nsMemMaximum,
 	}
 	return
 }
 
-// minResourceLimits finds the Min limit for resourceName. Nil is
-// returned if limitRanges is empty or limits contains no resourceName
-// limits.
-func minResourceLimits(limitRanges []*corev1.LimitRange, resourceName corev1.ResourceName) *resource.Quantity {
-	limits := []*resource.Quantity{}
+// GetMinMax finds the Minimum and Maximum limit for respectively for the specified resource.
+// Nil is returned if limitRanges is empty or limits contains no resourceName limits.
+func GetMinMax(limitRanges []*corev1.LimitRange, resourceName corev1.ResourceName) (minimum *resource.Quantity, maximum *resource.Quantity) {
+	minList, maxList := findMinMaxLimits(limitRanges, resourceName)
+
+	minimum = minQuantity(minList)
+	maximum = maxQuantity(maxList)
+
+	return
+}
+
+func findMinMaxLimits(limitRanges []*corev1.LimitRange, resourceName corev1.ResourceName) (minimum []*resource.Quantity, maximum []*resource.Quantity) {
+	minimum = []*resource.Quantity{}
+	maximum = []*resource.Quantity{}
 
 	for _, limitRange := range limitRanges {
-		for _, limit := range limitRange.Spec.Limits {
-			if limit.Type == corev1.LimitTypeContainer {
-				if limit, found := limit.Min[resourceName]; found {
-					clone := limit.DeepCopy()
-					limits = append(limits, &clone)
+		for _, limits := range limitRange.Spec.Limits {
+			if limits.Type == corev1.LimitTypeContainer {
+				if min, found := limits.Min[resourceName]; found {
+					clone := min.DeepCopy()
+					minimum = append(minimum, &clone)
+				}
+
+				if max, found := limits.Max[resourceName]; found {
+					clone := max.DeepCopy()
+					maximum = append(maximum, &clone)
 				}
 			}
 		}
 	}
 
-	if len(limits) == 0 {
-		return nil
-	}
-
-	return minQuantity(limits)
+	return
 }
 
 func minQuantity(quantities []*resource.Quantity) *resource.Quantity {
-	min := quantities[0].DeepCopy()
+	if len(quantities) == 0 {
+		return nil
+	}
+
+	min := *quantities[0]
 
 	for i := range quantities {
 		if quantities[i].Cmp(min) < 0 {
-			min = quantities[i].DeepCopy()
+			min = *quantities[i]
 		}
 	}
 
-	return &min
+	copy := min.DeepCopy()
+	return &copy
+}
+
+func maxQuantity(quantities []*resource.Quantity) *resource.Quantity {
+	if len(quantities) == 0 {
+		return nil
+	}
+
+	max := *quantities[0]
+
+	for i := range quantities {
+		if quantities[i].Cmp(max) > 0 {
+			max = *quantities[i]
+		}
+	}
+
+	copy := max.DeepCopy()
+	return &copy
 }
