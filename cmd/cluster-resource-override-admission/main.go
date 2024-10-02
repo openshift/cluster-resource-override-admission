c18cc25173f8c1a442914af47e1d550ce0aaed14
John Kyros <jkyros@redhat.com>
2024-10-01 22:56:13 -0500

    Fix klogv2 flags not being initialized
    This only matters on 4.13. When we bumped deps, generic-admission-server
    dragged in klogv2, but in the version that's compatible with our 1.26.1
    deps, it doesn't initialize the log flags right, so the binary reports
    they are unknown and will send pods into CrashLoopBackoff.
    
    This just force-initializes the log flags before the admission server
    runs so they are no longer unknown.
    
    Signed-off-by: John Kyros <jkyros@redhat.com>

--- cmd/cluster-resource-override-admission/main.go
+++ cmd/cluster-resource-override-admission/main.go
@@ -2,8 +2,14 @@ package main
 
 import (
 	"github.com/openshift/generic-admission-server/pkg/cmd"
+	"k8s.io/klog/v2"
 )
 
 func main() {
+	// jkyros: This is present only in 4.13 because the generic-admission-server switched to klogv2, and
+	// the only version of generic-admission-server we have that's compatible with our kube 1.26.1 deps
+	// doesn't initialize the flags right, so we initialize them here first so they aren't "unknown" and
+	// won't cause the pod to crash.
+	klog.InitFlags(nil)
 	cmd.RunAdmissionServer(&clusterResourceOverrideHook{})
 }
